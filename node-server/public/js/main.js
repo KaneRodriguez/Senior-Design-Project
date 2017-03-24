331359602d1343a4400baf02f7af327caedc8214
= <=>
2017-03-23 19:56:02 -0500

    Dock initital

--- node-server/public/js/main.js
+++ node-server/public/js/main.js
@@ -51,3 +51,16 @@ window.addEventListener("load", function() {
 document.getElementById('slider').addEventListener('input', function() {
 	document.body.style.opacity = this.value;
 });
+$(document).ready(function(){
+ 			$("ul.osx-dock li").each(function (type) {
+		     	$(this).hover(function () {
+		      		$(this).prev("li").addClass("nearby");
+		      		$(this).next("li").addClass("nearby");
+		     	},
+		     	function () {
+		      		$(this).prev("li").removeClass("nearby");
+		      		$(this).next("li").removeClass("nearby");
+		     	});
+		    });
+		});
+		
