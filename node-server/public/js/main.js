87b8bede9a36d16d8047066bd4bd6233705f0c5d
= <=>
2017-03-23 20:08:50 -0500

    Fix bulb

--- node-server/public/js/main.js
+++ node-server/public/js/main.js
@@ -51,3 +51,21 @@ window.addEventListener("load", function() {
 document.getElementById('slider').addEventListener('input', function() {
 	document.body.style.opacity = this.value;
 });
+
+$("#invert").on("click", function() {
+	$("body").toggleClass("inverted");
+	$(this).toggleClass("ion-ios-lightbulb ion-ios-lightbulb-outline");
+});
+
+$(document).ready(function(){
+ 			$("ul.osx-dock li").each(function (type) {
+		     	$(this).hover(function () {
+		      		$(this).prev("li").addClass("nearby");
+		      		$(this).next("li").addClass("nearby");
+		     	},
+		     	function () {
+		      		$(this).prev("li").removeClass("nearby");
+		      		$(this).next("li").removeClass("nearby");
+		     	});
+		    });
+		});
