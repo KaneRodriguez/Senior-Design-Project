9e4d7b22a2e5d854aa524758a97d670bebc64914
Kane Rodriguez <kaustez@siu.edu>
2017-04-16 20:02:26 -0500

    Add files via upload

---
+++ motor-control/motorControl/MTAVServo.h
@@ -0,0 +1,134 @@
+#ifndef MTAVSERVO
+#define MTAVSERVO
+
+#include <ArduinoJson.h>
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
+
+class MTAVServo
+{
+  public:
+    // Constructor
+    MTAVServo(String id, int boardNum, int positionPercentage, Adafruit_PWMServoDriver* controller, int Min, int Max); // position in terms of percentage of 100
+
+    // Destructor
+    ~MTAVServo() {}
+
+    // Get
+    String getServoId() {
+      return this->itsId;
+    }
+    int getServoPositionPercentage() {
+      return this->itsPositionPercentage;
+    }
+    int getBoardNumber() {
+      return this->itsBoardNumber;
+    }
+    int getMin() { return this->itsMin;}
+    int getMax() { return this->itsMax;}
+
+    
+    // Set
+    void setServoId(String id);
+    void setServoPositionPercentage(int positionPercentage);
+    void setBoardNumber(int boardNum) {
+      this->itsBoardNumber = boardNum;
+    }
+    void setServoMin(int Min);
+    void setServoMax(int Max);
+    // functionality
+
+    void updateServo();
+    void displayServoSpecs();
+    bool recieveSerialServoUpdates(JsonObject& root);
+
+Adafruit_PWMServoDriver* controller;
+  private:
+    String itsId; // armServo
+    int itsBoardNumber; // 0 -> 15
+    int itsPositionPercentage; // 0 to 100
+    int itsMin; // found by iteration (different per servo)
+    int itsMax; // found by iteration (different per servo)
+};
+
+MTAVServo::MTAVServo(String id, int boardNumber, int positionPercentage, Adafruit_PWMServoDriver * controller, int Min, int Max) {
+  this->controller = controller;
+  this->setServoId(id);
+  this->setServoPositionPercentage(positionPercentage);
+  this->setBoardNumber(boardNumber);
+  
+  this->setServoMin(Min);
+  this->setServoMax(Max);
+  
+  // NOTE: DO NOT UPDATE HERE
+}
+
+void MTAVServo::setServoId(String id) {
+  this->itsId = id;
+}
+
+void MTAVServo::setServoPositionPercentage(int positionPercentage) {
+  this->itsPositionPercentage = ( positionPercentage >= 100 ? 100 :  ( positionPercentage <= 0 ? 0 : positionPercentage ) ); // between 0 and 100 percent (obviously)
+}
+void MTAVServo::setServoMin(int Min) {
+  this->itsMin = Min;
+}
+void MTAVServo::setServoMax(int Max) {
+  this->itsMax = Max;
+}
+
+void MTAVServo::updateServo() {
+  // (board number, ?????????, pulse length)
+
+  // quickly turn the percentage into actual value
+
+  int difference = this->getMax() - this->getMin(); //MAX_MTAVSERVO_POSITION - MIN_MTAVSERVO_POSITION;
+  int pos = this->getServoPositionPercentage() * difference / 100 + this->getMin(); //MIN_MTAVSERVO_POSITION;
+    Serial.println(this->getServoPositionPercentage());
+
+  Serial.println(this->getMax());
+
+  Serial.println(this->getMin());
+
+  Serial.println(pos);
+  this->controller->setPWM(this->getBoardNumber(), 0, pos); // TODO: Why 0?????????
+}
+
+void MTAVServo::displayServoSpecs() {
+  Serial.println("Servo Id: " + this->getServoId());
+  Serial.print("Position Percentage: ");
+  Serial.print(this->getServoPositionPercentage());
+  Serial.print("%\n");
+  Serial.print("Board Number: ");
+  Serial.print(this->getBoardNumber());
+  Serial.print("%\n");
+  Serial.print("Min Pulse Length: ");
+  Serial.print(this->getMin());
+  Serial.print("%\n");
+  Serial.print("Max Pulse Length: ");
+  Serial.print(this->getMax());
+}
+
+bool MTAVServo::recieveSerialServoUpdates(JsonObject& root) {
+
+  // Test if parsing succeeds.
+  if (!root.success()) {
+    // Serial.println("parseObject() failed");
+    return false;
+  }
+
+  // {"id": "shoulder", "positionPercentage": 50}
+
+  // Fetch values.
+  const char * id = root["id"];
+  int positionPercentage = root["positionPercentage"];
+   Serial.println(id);
+  Serial.println(positionPercentage);
+  this->setServoPositionPercentage(positionPercentage);
+  // this->sendSerialServoSpecs();
+
+  return true;
+}
+#endif
+
+
--- motor-control/motorControl/motorControl.ino
+++ motor-control/motorControl/motorControl.ino
@@ -1,22 +1,22 @@
+#include <ArduinoJson.h>
 #include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
-#include <ArduinoJson.h>
-
 /******************* Initialize Global Variables ********************/
 
 #include "MTAVMotor.h"
+#include "MTAVServo.h"
 
 MTAVMotor motorA("motorA", "forward", 0);
 MTAVMotor motorB("motorB", "forward", 0);
 
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 
-#define SERVOMIN  150 // this is the 'minimum' pulse length count (out of 4096)
-#define SERVOMAX  600 // this is the 'maximum' pulse length count (out of 4096)
-
-
-
-
+MTAVServo claw("claw", 0, 0, &pwm, 120, 420);
+MTAVServo elbow("elbow", 1, 0, &pwm, 120, 440);
+MTAVServo shoulder("shoulder", 2, 0, &pwm, 120, 440);
+MTAVServo camx("camx", 3, 0, &pwm, 120, 440);
+MTAVServo camy("camy", 4, 0, &pwm, 120, 440);
+MTAVServo metald("metald", 5, 0, &pwm, 120, 440);
 
 String userCommand = "";
 
@@ -24,37 +24,38 @@ String userCommand = "";
 
 /*********************** General Setup ***********************/
 void setup() {
-    Serial.begin(9600);      // open the serial port at 9600 bps:
-    //  pwm.begin();                //FOR SERVOS
-    //  pwm.setPWMFreq(60);         //FOR SERVOS
-    
-    motorA.setMotorLpwmRpwmEnDisPins(11, 3, 2, 4);    
-    motorB.setMotorLpwmRpwmEnDisPins(10, 9, 8, 7);    
+  Serial.begin(9600);      // open the serial port at 9600 bps:
+  Serial.println("Arduino setting up! \n");
 
-    motorA.updateMotor();
-    motorB.updateMotor();
-    
-    
-    
-    
-    // reserve 200 bytes for the userCommand (consider lowering):
-    userCommand.reserve(200);
-    
-    Serial.println("Arduino set up! \n");
-    
-    // motorA.sendSerialMotorSpecs();
+  pwm.begin();
+  
+  pwm.setPWMFreq(60);  // Analog servos run at ~60 Hz updates
 
- pwm.begin();
-   
-   pwm.setPWMFreq(60);  // Analog servos run at ~60 Hz updates
- 
-   yield();
+  yield();
+  
+  motorA.setMotorLpwmRpwmEnDisPins(11, 3, 2, 4);
+  motorB.setMotorLpwmRpwmEnDisPins(10, 9, 8, 7);
+
+  motorA.updateMotor();
+  motorB.updateMotor();
+
+  claw.updateServo();
+  elbow.updateServo();
+  shoulder.updateServo();
+  camx.updateServo();
+  camy.updateServo();
+  metald.updateServo();
+
+  // reserve 200 bytes for the userCommand (consider lowering):
+  userCommand.reserve(200);
+
+  Serial.println("Arduino set up! \n");
+
+  // motorA.sendSerialMotorSpecs();
+
+  delay(3000);
 
 
-        
-    delay(3000);
-    
- 
 }
 /*********************** End General Setup ***********************/
 
@@ -63,8 +64,17 @@ void setup() {
 void loop() { // main
 
   // update motors
-    motorA.updateMotor();
-    motorB.updateMotor();
+  motorA.updateMotor();
+  motorB.updateMotor();
+
+  // update servos
+  claw.updateServo();
+  elbow.updateServo();
+  shoulder.updateServo();
+  camx.updateServo();
+  camy.updateServo();
+  metald.updateServo();
+  
 }
 
 /**************************** End Main **********************************/
@@ -76,32 +86,45 @@ bool commandReceived(char json[200]) {
   StaticJsonBuffer<200> jsonBuffer;
 
   JsonObject& root = jsonBuffer.parseObject(json);
+      Serial.println("bout to parse obje");
 
   // Test if parsing succeeds.
   if (!root.success()) {
-    // Serial.println("parseObject() failed");
+   // Serial.println("parseObject() failed");
     return false;
   }
+      Serial.println("parsed obje");
 
- /* const char * id = root["id"]; // TODO: error check if there is an "id" attribute
-
-  // TODO: make switch lookup table
-  if(strcmp(id,"motorA")==0) {
-    motorA.recieveSerialMotorUpdates(root); // todo, pass in the JsonObject to the function
-  }
-  if(strcmp(id,"motorB")==0) {
-    motorB.recieveSerialMotorUpdates(root); // todo, pass in the JsonObject to the function
-  }
-  */
-  int speedPercentage = root["s"];
-  root.printTo(Serial);  
-  float difference = SERVOMAX - SERVOMIN;
-  float position = difference * speedPercentage + SERVOMIN;
-  
-  Serial.print("Moving to position" + speedPercentage);
-  Serial.print(speedPercentage);
-  pwm.setPWM(0, 0, 60);
+  const char * id = root["id"]; // TODO: error check if there is an "id" attribute
  
+  
+    // TODO: make switch lookup table
+    if(strcmp(id,"motorA")==0) {
+     motorA.recieveSerialMotorUpdates(root); // todo, pass in the JsonObject to the function
+    }
+    if(strcmp(id,"motorB")==0) {
+     motorB.recieveSerialMotorUpdates(root); // todo, pass in the JsonObject to the function
+    }
+    
+    if(strcmp(id,"claw")==0) {
+     claw.recieveSerialServoUpdates(root); // todo, pass in the JsonObject to the function
+    }                                                                                           //{"id":"claw", "positionPercentage":30}
+    if(strcmp(id,"elbow")==0) {
+     elbow.recieveSerialServoUpdates(root); // todo, pass in the JsonObject to the function
+    }
+    if(strcmp(id,"shoulder")==0) {
+     shoulder.recieveSerialServoUpdates(root); // todo, pass in the JsonObject to the function
+    }
+    if(strcmp(id,"camx")==0) {
+     camx.recieveSerialServoUpdates(root); // todo, pass in the JsonObject to the function
+    }
+    if(strcmp(id,"camy")==0) {
+     camy.recieveSerialServoUpdates(root); // todo, pass in the JsonObject to the function
+    }
+    if(strcmp(id,"metald")==0) {
+     metald.recieveSerialServoUpdates(root); // todo, pass in the JsonObject to the function
+    }
+
   return true;
 }
 
@@ -111,20 +134,20 @@ void serialEvent() {
   while (Serial.available()) {
     // get the new byte:
     char inChar = (char)Serial.read();
-    
+
     userCommand += inChar;
-    
+
     // add it to the inputString:
     if (inChar == '}') { // could potentially be the end of the object
       char json[200];
       userCommand.toCharArray(json, 200);
       
-      if(commandReceived(json)) {
-        Serial.println("CR");
-        userCommand = ""; 
+      if (commandReceived(json)) {
+        // Serial.println("CR");
+        userCommand = "";
         delay(100);
       } else {
-        // Serial.println("Command not received");
+        Serial.println("Command not received");
       }
     }
   }
